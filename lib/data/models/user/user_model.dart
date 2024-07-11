87517ea00a4feecba656d25f5699a0febf145965
Ritik6559 <ritikjoshi741@gmail.com>
2024-07-11 16:15:08 +0530

    adding user model

---
+++ lib/data/models/user/user_model.dart
@@ -0,0 +1,39 @@
+
+class UserModel {
+  final String id;
+  final String name;
+  final String email;
+  UserModel({
+    required this.id,
+    required this.name,
+    required this.email,
+  });
+
+  UserModel copyWith({
+    String? id,
+    String? name,
+    String? email,
+  }) {
+    return UserModel(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      email: email ?? this.email,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      id: map['id'] ?? '',
+      name: map['name'] ?? '',
+      email: map['email'] ?? '',
+    );
+  }
+}
